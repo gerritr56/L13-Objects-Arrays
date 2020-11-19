500756697ccbc5d1b68715d5623bf2b46b3820d0
gerritr56 <68347062+gerritr56@users.noreply.github.com>
2020-11-19 13:10:24 +0100

    Add files via upload

---
+++ 1-object/index.js
@@ -0,0 +1,14 @@
+let person = {
+    name: 'Gerrit',
+    age: 63,
+    evaluations: [ 7, 10, 9]
+};
+console.log (person);
+
+console.log (person.name);
+console.log (person.age);
+
+console.log (person ['name'] );
+console.log (person ['age'] );
+
+console.log (person.evaluations);
---
+++ 2-arrays/index.js
@@ -0,0 +1,15 @@
+let colors = ['groen', 'blauw', 'rood'];
+let lastNum = [colors.length - 1];
+let total = colors.push('geel', 5, {greeting:"Hi ik ben een object"});
+
+
+console.log (colors);
+
+console.log (colors.length);
+
+console.log (colors [0]);
+
+
+console.log (colors [lastNum]);
+
+console.log (colors.slice(-1));
